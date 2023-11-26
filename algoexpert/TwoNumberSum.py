fe7e804f5d54a8daf5730542da4a077a05016ece
mrtsfn601 <80962941+mrtsfn601@users.noreply.github.com>
2023-11-26 08:37:28 -0500

    Update TwoNumberSum.py

--- algoexpert/TwoNumberSum.py
+++ algoexpert/TwoNumberSum.py
@@ -12,3 +12,17 @@ def twoNumberSum(array, targetSum):
             return [targetSum - num, num]
         visited.add(num)
     return []
+
+def twoNumberSum(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currSum = array[left] + array[right]
+        if currSum == targetSum:
+            return [array[left], array[right]]
+        elif currSum < targetSum:
+            left += 1
+        elif currSum > targetSum:
+            right -= 1
+    return []
