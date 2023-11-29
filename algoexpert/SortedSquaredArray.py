ac699eb91fb4e385a8f72de25dba10b04d2c6080
mrtsfn601 <80962941+mrtsfn601@users.noreply.github.com>
2023-11-28 21:28:51 -0500

    Create SortedSquaredArray.py

---
+++ algoexpert/SortedSquaredArray.py
@@ -0,0 +1,55 @@
+# O(n log(n)) time | O(n) space
+def sortedSquaredArray(array):
+    squared = [0] * len(array)
+    i = 0
+    while i < len(array):
+        squared[i] = array[i] * array[i]
+        i += 1
+    squared.sort()
+    return squared
+
+# O(n log(n)) time | O(n) space
+def sortedSquaredArray(array):
+    squared = [0 for _ in array]
+    for i in range(len(array)):
+        squared[i] = array[i] * array[i]
+    squared.sort()
+    return squared
+
+# O(n) time | O(n) space
+def sortedSquaredArray(array):
+    size = len(array)
+    squared = [0] * size
+    i = size - 1
+    left = 0
+    right = size - 1
+    while left <= right:
+        leftCurr = array[left] * array[left]
+        rightCurr = array[right] * array[right]
+        if leftCurr <= rightCurr:
+            squared[i] = rightCurr
+            right -= 1
+        else:
+            squared[i] = leftCurr
+            left += 1
+        i -= 1
+    return squared
+
+# O(n) time | O(n) space
+def sortedSquaredArray(array):
+    squared = [0 for _ in array]
+    left = 0
+    right = len(array) - 1
+    
+    for i in reversed(range(len(array))):
+        leftCurr = array[left] * array[left]
+        rightCurr = array[right] * array[right]
+        
+        if leftCurr <= rightCurr:
+            squared[i] = rightCurr
+            right -= 1
+        else:
+            squared[i] = leftCurr
+            left += 1
+    
+    return squared
