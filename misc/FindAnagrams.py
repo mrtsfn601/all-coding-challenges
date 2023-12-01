d1827790537ddf9e17447dad63e1804ef2512db1
mrtsfn601 <80962941+mrtsfn601@users.noreply.github.com>
2023-11-30 21:53:48 -0500

    Update FindAnagrams.py

--- misc/FindAnagrams.py
+++ misc/FindAnagrams.py
@@ -1,7 +1,8 @@
 def find_anagrams(word, words):
     word_normalized = sorted(word.strip().lower())
+    length = len(word_normalized)
     words_normalized = {w.strip().lower() for w in words}
-    return sorted({word for word in words_normalized if word_normalized == sorted(word)})
+    return sorted({word for word in words_normalized if length == len(word) and word_normalized == sorted(word)})
 
 %time find_anagrams("stop", ["tosp", "Opst", "opst", "post ", "posst", "tops\n"])
 
