36f8d69f9bd4d64ec684598105cfbec889f78e7c
Marat Safin <80962941+mrtsfn601@users.noreply.github.com>
2023-12-02 23:39:54 -0500

    Create FindClosestValueInBST.py

---
+++ algoexpert/FindClosestValueInBST.py
@@ -0,0 +1,20 @@
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstRecursive(tree, target, closest = tree.value)
+
+def findClosestValueInBstRecursive(tree, target, closest):
+    if tree is None:
+        return closest
+    if abs(tree.value - target) < abs(closest - target):
+        closest = tree.value
+    if tree.left is not None:
+        closest = findClosestValueInBstRecursive(tree.left, target, closest)
+    if tree.right is not None:
+        closest = findClosestValueInBstRecursive(tree.right, target, closest)    
+    return closest
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
