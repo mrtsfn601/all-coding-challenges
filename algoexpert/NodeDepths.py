59607003eaeb0fa6f7cde627decc670cab734e44
Marat Safin <80962941+mrtsfn601@users.noreply.github.com>
2023-12-04 21:29:28 -0500

    Create NodeDepths.py

---
+++ algoexpert/NodeDepths.py
@@ -0,0 +1,24 @@
+# O(n) time | O(n) space
+# where n is the number of nodes in binary tree
+def nodeDepths(root, depths = 0, sum = 0):
+    if root.left is not None:
+        sum = nodeDepths(root.left, depths + 1, sum + depths + 1)
+    if root.right is not None:
+        sum = nodeDepths(root.right, depths + 1, sum + depths + 1)
+    return sum
+
+
+# O(n) time | O(n) space
+# where n is the number of nodes in binary tree
+def nodeDepths(root, depth = 0):
+    if root is None:
+        return 0
+    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
