f49235ca5ebe0ca76571acff0cf131867ddf261e
Marat Safin <80962941+mrtsfn601@users.noreply.github.com>
2024-01-08 05:32:23 -0500

    Create MoveElementToEnd.py

---
+++ algoexpert/MoveElementToEnd.py
@@ -0,0 +1,14 @@
+# O (n) time | O(1) space where n is the length of the array
+def moveElementToEnd(array, toMove):
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        if array[right] == toMove:
+            right -= 1
+        elif array[left] == toMove:
+            array[left], array[right] = array[right], array[left]
+            left += 1
+            right -= 1
+        else:
+            left += 1
+    return array
