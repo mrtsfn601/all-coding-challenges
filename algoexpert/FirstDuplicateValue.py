2a998c92a266905b862a71a04b486e74c2fa58ae
Marat Safin <80962941+mrtsfn601@users.noreply.github.com>
2024-01-13 06:24:06 -0500

    Update FirstDuplicateValue.py

--- algoexpert/FirstDuplicateValue.py
+++ algoexpert/FirstDuplicateValue.py
@@ -6,3 +6,13 @@ def firstDuplicateValue(array):
             return num
         visited.add(num)
     return -1
+
+
+# O(n) time | O(1) space where n in the number of elements in the array
+def firstDuplicateValue(array):
+    for val in array:
+        absVal = abs(val)
+        if array[absVal - 1] < 0:
+            return absVal
+        array[absVal - 1] *= -1
+    return -1
