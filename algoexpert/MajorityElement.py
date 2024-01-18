3ba3752518a86a35cf97212b95290c10908c274b
Marat Safin <80962941+mrtsfn601@users.noreply.github.com>
2024-01-18 06:35:32 -0500

    Update MajorityElement.py

--- algoexpert/MajorityElement.py
+++ algoexpert/MajorityElement.py
@@ -1,19 +1,3 @@
-# O((n/2)^2) time | O(1) space
-def majorityElement(array):
-    maj_val = array[0]
-    maj_count = 0
-    for i in range(len(array) // 2 + 1):
-        curr_count = 0
-        for j in range(i + 1, len(array)):
-            if array[i] == array[j]:
-                curr_count += 1
-            if maj_count < curr_count:
-                maj_count, maj_val = curr_count, array[i]
-                if len(array) // 2 < maj_count:
-                    return maj_val
-    return maj_val
-
-
 # O(n) time | O(1) space
 def majorityElement(array):
     count = 0
